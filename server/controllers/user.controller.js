0be890b667e6b6991e30afe6a3d0d6742f6889ab
jaykosle <130846062+jaykosle@users.noreply.github.com>
2025-06-16 20:27:31 +0530

    login,auth and logout functionalities

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,7 +1,9 @@
 import sendEmail from '../config/sendEmail.js'
-import userModel from '../models/user.model.js'
+import UserModel from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
 import verifyEmailTemplate from '../utils/verifyEmailTemplate.js'
+import generatedAccessToken from '../utils/generatedAccessToken.js'
+import generatedRefreshToken from '../utils/generatedRefreshToken.js'
 
 export async function registerUserController(request,response) {
     try {
@@ -15,11 +17,11 @@ export async function registerUserController(request,response) {
             })
         }
 
-        const user = await userModel.findOne({email})
+        const user = await UserModel.findOne({email})
 
         if(user){
             return response.status(500).json({
-                message : error.message || error,
+                message :  "Already register email",
                 error : true,
                 success : false
             })
@@ -53,6 +55,144 @@ export async function registerUserController(request,response) {
             error : false,
             success : true,
             data : save
+        })
+
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+}
+
+export async function verifyEmailController(request,response){
+    try {
+        const {code} = request.body
+
+        const user = await response.UserModel.findOne({_id : code})
+
+        if(!user){
+            return response.status(400).json({
+                message:"Invalid code",
+                error : true,
+                success : false
+            })
+        }
+
+        const updateUser = await response.UserModel.findOne({_id : code},{
+            verify_email : true
+        })
+
+        return response.status(500).json({
+            message :"Verification successful",
+            error : false,
+            success : true
+        })
+
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+}
+
+//login
+export async function loginController(request,response){
+    try {
+        const { email , password } = request.body
+
+
+        if(!email || !password){
+            return response.status(400).json({
+                message : "provide email, password",
+                error : true,
+                success : false
+            })
+        }
+
+        const user = await UserModel.findOne({ email })
+
+        if(!user){
+            return response.status(400).json({
+                message : "User not register",
+                error : true,
+                success : false
+            })
+        }
+
+        if(user.status !== "Active"){
+            return response.status(400).json({
+                message : "Contact to Admin",
+                error : true,
+                success : false
+            })
+        }
+
+        const checkPassword = await bcryptjs.compare(password,user.password)
+
+        if(!checkPassword){
+            return response.status(400).json({
+                message : "Check your password",
+                error : true,
+                success : false
+            })
+        }
+
+        const accesstoken = await generatedAccessToken(user._id)
+        const refreshToken = await generatedRefreshToken(user._id)
+
+        const cookiesOption = {
+            httpOnly : true,
+            secure : true,
+            sameSite : "None"
+        }
+        response.cookie('accessToken',accesstoken,cookiesOption)
+        response.cookie('refreshToken',refreshToken,cookiesOption)
+
+        return response.json({
+            message : "Login successfully",
+            error : false,
+            success : true,
+            data : {
+                accesstoken,
+                refreshToken
+            }
+        })
+
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+}
+
+//logout
+export async function logoutController(request,response){
+    try {
+        const userid = request.userId
+
+        const cookiesOption = {
+            httpOnly : true,
+            secure : true,
+            sameSite : "None"
+        }
+
+        response.clearCookie('accessToken',cookiesOption)
+        response.clearCookie('refreshToken',cookiesOption)
+
+        const removeRefreshToken = await UserModel.findByIdAndUpdate(userid,{
+            refresh_token :""
+        })
+
+        return response.json({
+            message : "Logout successfully",
+            error : false,
+            success : true
         })
 
     } catch (error) {

640c6405956e265fb273278c0a6d43c92c954b0f
jaykosle <130846062+jaykosle@users.noreply.github.com>
2025-06-17 17:22:03 +0530

    all user controller are done

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -4,6 +4,11 @@ import bcryptjs from 'bcryptjs'
 import verifyEmailTemplate from '../utils/verifyEmailTemplate.js'
 import generatedAccessToken from '../utils/generatedAccessToken.js'
 import generatedRefreshToken from '../utils/generatedRefreshToken.js'
+import uploadImageCloudinary from '../utils/uploadImageCloudinary.js'
+import { response } from 'express'
+import forgotPasswordTemplate from '../utils/forgotPasswordTemplate.js'
+import generatedOtp from '../utils/generatedOtp.js'
+import jwt from 'jsonwebtoken'
 
 export async function registerUserController(request,response) {
     try {
@@ -193,6 +198,286 @@ export async function logoutController(request,response){
             message : "Logout successfully",
             error : false,
             success : true
+        })
+
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+}
+
+//uploading avatar
+export async function uploadAvatar(request,response) {
+    try {
+        const userId = request.userId //from auth
+        const image = request.file //from multer
+
+        const upload = await uploadImageCloudinary(image)
+
+        const updateUser = await UserModel.findByIdAndUpdate(userId,{
+            avatar : upload.url
+        })
+
+        return response.json({
+            message : "Image uploaded successfully",
+            error : false,
+            success : true,
+            data : {
+                _id : userId,
+                avatar : upload.url
+            }
+        })
+
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+}
+
+//update user details
+export async function updateUserDetails(request,response) {
+    try {
+        const userId = request.userId
+        const {name, email, mobile, password} = request.body
+
+        let hashPassword = ""
+
+        if(password){
+            const salt = await bcryptjs.genSalt(10)
+            const hashPassword = await bcryptjs.hash(password,salt)
+        }
+
+        const updateUser = await UserModel.updateOne({_id : userId},{
+            ...(name && {name : name}),
+            ...(email && { email : email }),
+            ...(mobile && { mobile : mobile }),
+            ...(password && { password : hashPassword })
+        })
+
+        return response.json({
+            message : "Updated successfully",
+            error : false,
+            success : true,
+            data : updateUser
+        })
+
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+}
+
+//forgot password
+export async function forgotPasswordController(request,response) {
+    try {
+        const { email } = request.body 
+
+        const user = await UserModel.findOne({ email })
+
+        if(!user){
+            return response.status(400).json({
+                message : "Email not available",
+                error : true,
+                success : false
+            })
+        }
+
+        const otp = generatedOtp()
+        const expireTime = new Date() + 60 * 60 * 1000 // 1hr
+
+        const update = await UserModel.findByIdAndUpdate(user._id,{
+            forgot_password_otp : otp,
+            forgot_password_expiry : new Date(expireTime).toISOString()
+        })
+
+        await sendEmail({
+            sendTo : email,
+            subject :"Forgot password from Binkeyit",
+            html : forgotPasswordTemplate({
+                name : user.name,
+                otp : otp
+            })
+        })
+
+        return response.json({
+            message : "check your email",
+            error : false,
+            success : true
+        })
+
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+}
+
+//verify forget password otp
+export async function verifyForgetPasswordOtp(request,response) {
+    try {
+        const {email, otp} = request.body
+
+        if(!email || !otp){
+            return response.status(400).json({
+                message : "Provide required field email, otp.",
+                error : true,
+                success : false
+            })
+        }
+
+        const user = await UserModel.findOne({ email })
+
+        if(!user){
+            return response.status(400).json({
+                message : "Email not available",
+                error : true,
+                success : false
+            })
+        }
+
+        const currentTime = new Date().toISOString()
+
+        if(user.forgot_password_expiry < currentTime  ){
+            return response.status(400).json({
+                message : "Otp is expired",
+                error : true,
+                success : false
+            })
+        }
+
+        if(otp !== user.forgot_password_otp){
+            return response.status(400).json({
+                message : "Invalid otp",
+                error : true,
+                success : false
+            })
+        }
+
+        //if otp is not expired
+        //otp === user.forgot_password_otp
+
+        const updateUser = await UserModel.findByIdAndUpdate(user?._id,{
+            forgot_password_otp : "",
+            forgot_password_expiry : ""
+        })
+        
+        return response.json({
+            message : "Verify otp successfully",
+            error : false,
+            success : true
+        })
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+}
+
+//reset the password
+export async function resetPassword(request,response) {
+    try {
+        const { email , newPassword, confirmPassword } = request.body 
+
+        if(!email || !newPassword || !confirmPassword){
+            return response.status(400).json({
+                message : "provide required fields email, newPassword, confirmPassword"
+            })
+        }
+
+        const user = await UserModel.findOne({ email })
+
+        if(!user){
+            return response.status(400).json({
+                message : "Email is not available",
+                error : true,
+                success : false
+            })
+        }
+
+        if(newPassword !== confirmPassword){
+            return response.status(400).json({
+                message : "newPassword and confirmPassword must be same.",
+                error : true,
+                success : false,
+            })
+        }
+
+        const salt = await bcryptjs.genSalt(10)
+        const hashPassword = await bcryptjs.hash(newPassword,salt)
+
+        const update = await UserModel.findOneAndUpdate(user._id,{
+            password : hashPassword
+        })
+
+        return response.json({
+            message : "Password updated successfully.",
+            error : false,
+            success : true
+        })
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
+        })
+    }
+}
+
+//refresh token controller
+export async function refreshToken(request,response) {
+    try {
+        const refreshToken = request.cookies.refreshToken || request?.headers?.authorization?.split(" ")[1]  /// [ Bearer token]
+
+        if(!refreshToken){
+            return response.status(401).json({
+                message : "Invalid token",
+                error  : true,
+                success : false
+            })
+        }
+
+        const verifyToken = await jwt.verify(refreshToken,process.env.SECRET_KEY_REFRESH_TOKEN)
+
+        if(!verifyToken){
+            return response.status(401).json({
+                message : "token is expired",
+                error : true,
+                success : false
+            })
+        }
+
+        const userId = verifyToken?._id
+
+        const newAccessToken = await generatedAccessToken(userId)
+
+        const cookiesOption = {
+            httpOnly : true,
+            secure : true,
+            sameSite : "None"
+        }
+
+        response.cookie('accessToken',newAccessToken,cookiesOption)
+
+        return response.json({
+            message : "New access token is generated",
+            error : false,
+            success : true,
+            data : {
+                accessToken : newAccessToken
+            }
         })
 
     } catch (error) {
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,6 +1,7 @@
 import {Router} from 'express'
-import { loginController, logoutController, registerUserController, verifyEmailController } from '../controllers/user.controller.js'
+import { forgotPasswordController, loginController, logoutController, refreshToken, registerUserController, resetPassword, updateUserDetails, uploadAvatar, verifyEmailController, verifyForgetPasswordOtp } from '../controllers/user.controller.js'
 import auth from '../middleware/auth.js'
+import upload from '../middleware/multer.js'
 
 const userRouter = Router()
 
@@ -8,5 +9,12 @@ userRouter.post('/register',registerUserController)
 userRouter.post('/verify-email', verifyEmailController)
 userRouter.post('/login',loginController)
 userRouter.get('/logout',auth,logoutController)
+userRouter.put('/upload-avatar',auth,upload.single('avatar'),uploadAvatar)
+userRouter.put('/update-user',auth,updateUserDetails)
+userRouter.put('/forgot-password',forgotPasswordController)
+userRouter.put('/verify-forgot-password-otp',verifyForgetPasswordOtp)
+userRouter.put('/reset-password',resetPassword)
+userRouter.post('/refresh-token',refreshToken)
+
 
 export default userRouter
